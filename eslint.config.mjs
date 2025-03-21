e4538f5ec264e3cb7951d12266949738f4ee8c86
JohnAchicaizaG <159038108+JohnAchicaizaG@users.noreply.github.com>
2025-03-20 23:47:55 -0500

    add conf prettier, tsup, modified packege.json add logger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,52 +7,52 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
-  {
-    languageOptions: {
-      globals: globals.node,
-    },
-  },
-  {
-    ignores: ["dist/"],
-  },
-  { files: ["src/**/*.{js,ts,jsx,tsx}", "tests/**/*.{js,ts,jsx,tsx}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.node },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js, import: importPlugin },
-    extends: ["js/recommended"],
-  },
-  tseslint.configs.recommended,
-  {
-    files: ["tests/**/*.{js,ts,jsx,tsx}"],
-    ...jest.configs["flat/recommended"],
-    rules: {
-      ...jest.configs["flat/recommended"].rules,
-      "jest/prefer-expect-assertions": "off",
-    },
-  },
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-empty-function": "error",
-      "prefer-const": "error",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      eqeqeq: ["warn", "always"],
-    },
-  },
-  eslintPluginPrettierRecommended,
-  {
-    settings: {
-      "import/resolver": {
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
+    {
+        languageOptions: {
+            globals: globals.node,
         },
-      },
     },
-  },
+    {
+        ignores: ["dist/"],
+    },
+    { files: ["src/**/*.{js,ts,jsx,tsx}", "tests/**/*.{js,ts,jsx,tsx}"] },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: { globals: globals.node },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: { js, import: importPlugin },
+        extends: ["js/recommended"],
+    },
+    tseslint.configs.recommended,
+    {
+        files: ["tests/**/*.{js,ts,jsx,tsx}"],
+        ...jest.configs["flat/recommended"],
+        rules: {
+            ...jest.configs["flat/recommended"].rules,
+            "jest/prefer-expect-assertions": "off",
+        },
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-empty-function": "error",
+            "prefer-const": "error",
+            "no-console": ["warn", { allow: ["warn", "error"] }],
+            eqeqeq: ["warn", "always"],
+        },
+    },
+    eslintPluginPrettierRecommended,
+    {
+        settings: {
+            "import/resolver": {
+                node: {
+                    extensions: [".js", ".jsx", ".ts", ".tsx"],
+                },
+            },
+        },
+    },
 ]);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export function suma(a: number, b: number) {
-  return a + b;
-}
+import express, { Request, Response } from "express";
+import { logger } from "@/config/logger";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta de prueba
+app.get("/", (req: Request, res: Response) => {
+    logger.info("🟢 Se recibió una solicitud GET en /");
+    res.json({ message: "🚀 Servidor Express con TypeScript funcionando!" });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    logger.info(`✅ Servidor corriendo en http://localhost:${PORT}`);
+});
+
+export {}; // fuerza que se genere un `.d.ts` válido
